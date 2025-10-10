43c267e26367d39e6fbc63e3ed07433ec55a41ea
Puneet Sharma <psharma051992@gmail.com>
2025-10-11 00:58:41 +0530

    New Files Added

---
+++ src/ex_02_Keywords_Identifiers_Variables/Lab004_Keywords.py
@@ -0,0 +1,13 @@
+# There are many keywords in the Python like
+# print()
+# class
+# None
+# -- To use all keywords and we have to import them
+import keyword
+print(keyword.kwlist)
+
+
+
+# below are set of keywords which we get the running the above code
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
---
+++ src/ex_02_Keywords_Identifiers_Variables/Lab005_Variable_Part1.py
@@ -0,0 +1,11 @@
+age = 30
+age = "Puneet"
+
+# Dynamic Type Language : It understands and print the latest value even if we have multiple same variables
+name = "Puneet"
+name = "Puneet"
+name = "Puneet"
+name = "Puneet"
+name = "Sharma"
+print(age)
+print(name)
