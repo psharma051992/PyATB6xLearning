32d359eefbeab62323dfcc9c821fb2df18e4dde5
Puneet Sharma <psharma051992@gmail.com>
2025-10-11 00:53:56 +0530

    New Files Added

---
+++ src/ex_02_Keywords_Identifiers_Variables/Lab006_Identifier.py
@@ -0,0 +1,14 @@
+age = 65
+_age = 65
+
+print(age)
+print(_age)
+
+_ = 65
+print(_)
+_ = _+1
+print(_)
+#Basic Rules
+# 1. Variable name can not be start with numbers
+# 2. Variable name will be alphanumeric
+
