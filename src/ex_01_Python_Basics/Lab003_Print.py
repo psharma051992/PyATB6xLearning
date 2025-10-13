443168ff2a691003b4146d1bbe423c74da8d9806
Puneet Sharma <psharma051992@gmail.com>
2025-10-14 03:31:40 +0530

    Python Basics

--- src/ex_01_Python_Basics/Lab003_Print.py
+++ src/ex_01_Python_Basics/Lab003_Print.py
@@ -8,11 +8,11 @@ print("Hello World!")
 # end='\n' is the new line but if you write (end="*") with any characters in the double quote then no new line is there
 
 print("Puneet", "belongs", "to", "Punjab")
-print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995)
+print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", '1995')
 print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995, True, 1.234)
 print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995, True, 1.234, sep='$')
-print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995, True, 1.234, sep='$',
-      end="_")  # if we use (end='\n') then new line will print in the console
+print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995, True, 1.234, sep='$',end="_")
+# if we use (end='\n') then new line will print in the console
 print("Puneet", "belongs", "to", "Punjab", "and", "has DOB", 1995, True, 1.234)
 
 # IndentationError: Unexpected Error (It comes when you add space at the start of the line and you can automatically correct it by using reformat code)
